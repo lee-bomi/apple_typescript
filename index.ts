d8028312558cb207fccedb295e3dd278c8e22f8b
emmababy <bomvll@naver.com>
2022-04-12 21:50:32 +0900

    add narrowing

--- index.ts
+++ index.ts
@@ -43,5 +43,84 @@ let 학교 :{
 학교.friend = ['Lee', 학교.teacher]
 
 
+//0412 함수
+//narrowing
+function 함수(x :number | string) :void {
+    if (typeof x === "number") {
+        console.log(x + 2);
+    }
+}
+
+함수(3);
+
+//숙제1
+function 숙제1(x? :string) :void {
+    if (typeof x === "string") {
+        console.log('안녕하세요 ' + x);
+    } else {
+        console.log('이름이 없습니다')
+    }
+}
+
+숙제1('이보미');
+
+//숙제2
+function 숙제2(x :string | number) :number {
+    if (typeof x === "string") {
+        return x.length;
+    }else {
+        return x.toString().length;
+    }
+}
+
+//숙제3
+function 숙제3(money :number, house :boolean | number, fascinate :string | number) :string {
+    house === true ? house=500 : house=0;
+    fascinate === '상' ? fascinate=100 : fascinate=0;
+    let total = money + house + fascinate;
+    if (total >= 600) {
+        return '결혼가능';
+    }
+}
+
+숙제3(700, true, '상');
 
 
+//narrowing = 타입확정!(typeof)
+//typeof귀찮으면 = assertion(타입 덮어쓰기)
+function 내함수(x: number | string) {
+    let array :number[] = [];
+    array[0] = x as number; //타입을 골라줄때 쓰기(타입 바꾸려고 쓰지말기)
+}
+
+//숙제4
+function 클리닝함수(x: (string | number)[]) :number[] {
+    let array :number[] = []
+    for (let i = 0; i < x.length; i++) {
+        if (typeof x[i] === 'string') {
+            let y = Number(x[i]);
+            array.push(y);
+        } else if(typeof x[i] === 'number') {
+            let z = x[i] as number;
+            array.push(z);
+        }
+    }
+
+    return array;
+}
+
+console.log('숙제4');
+console.log(클리닝함수(['1',2,'3']));
+
+//숙제5
+function lastSubject(x :{ subject : string | string[] }) :string{
+    if (x.subject === 'string') {
+        return x.subject;
+    } else if (Array.isArray(x.subject)) {
+        return x.subject[x.subject.length-1]
+    } else {
+        return '없음'
+    }
+
+    console.log(lastSubject({subject : ['english', 'art']}))
+}
